b5fe8620d4cf8a3d54821ec980246ccd158f5458
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-03-15 23:39:52 +0330

    Add mainclass for import images from src to dest

--- assets/src/js/ltr/home-core-ltr.js
+++ assets/src/js/ltr/home-core-ltr.js
@@ -1,8 +1,6 @@
 /**
  * Created by Jamal on 8/3/2019.
  */
-//import mainclass from './mainclass-ltr';
 import '../../sass/ltr/home-core-ltr.scss';
-
-
-//mainclass.importImages();
+import mainclass from './mainclass-ltr';
+mainclass.importImages();
--- assets/src/js/rtl/home-core-rtl.js
+++ assets/src/js/rtl/home-core-rtl.js
@@ -1,1 +1,3 @@
 import '../../sass/rtl/home-core-rtl.scss';
+import mainclass from './mainclass-rtl';
+mainclass.importImages();
